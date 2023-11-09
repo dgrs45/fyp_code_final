3fddd282e5afcaa3edd0805a4e3724c9347eb915
krishachemburkar <krishachemburkar446@gmail.com>
2023-11-09 23:42:34 +0530

    Latest Changes

--- api.py
+++ api.py
@@ -1,36 +1,29 @@
 from flask import Flask, request
 from flask_cors import CORS
 import numpy as np
+import torch
 
 
-
-
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s') 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/camera', methods=["POST","GET"])
 def get_frame():
     try:
-        cap = acapture.open(0) # /dev/video0
-        while True:
-            check,frame = cap.read() # non-blocking
-            print(check)
-            if check:
-                frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-                cv2.imshow("test",frame)
-                results = model(frame)
-                print('\n\nresult', type(results))
-                classes_detected = []
-                for bbox in results.xyxy[0]:
-                    xmin, ymin, xmax, ymax, confidence, class_id = bbox.tolist()
-                    class_name = model.names[int(class_id)]
-                    classes_detected.append(class_name)
-                    print(class_name)
-                cv2.waitKey(1)
-                if len(classes_detected) != 0:
-                        return {"main_class_detected": str(classes_detected[0])}
-                else:
-                    return {"main_class_detected": "No classes detected"}
+        frm = request.data
+        frame = np.array(frm)
+        results = model(frame)
+        classes_detected = []
+        for bbox in results.xyxy[0]:
+            xmin, ymin, xmax, ymax, confidence, class_id = bbox.tolist()
+            class_name = model.names[int(class_id)]
+            classes_detected.append(class_name)
+            print(class_name)
+        if len(classes_detected) != 0:
+                return {"main_class_detected": str(classes_detected[0])}
+        else:
+            return {"main_class_detected": "No classes detected"}
     except KeyboardInterrupt:
         pass
     return "hi"
--- raspi_get.py
+++ raspi_get.py
@@ -1,17 +1,22 @@
 import pyttsx3
 import acapture
-import torch
 import cv2
-
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s') 
-engine = pyttsx3.init()   
-engine.runAndWait()
 import requests
+
+engine = pyttsx3.init()   
+
 try:
+
+    cap = acapture.open(0) 
     while True:
-        res = requests.get('http://192.168.29.9:5000/camera')
-        print(res.json())
-        engine.say(res.json()['main_class_detected'])
-        engine.runAndWait()
+        check,frame = cap.read() # non-blocking
+        if check:
+            frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            
+            res = requests.post('http://192.168.29.9:5000/camera', data=frame.tolist())
+            engine.say(res.json()['main_class_detected'])
+            engine.runAndWait()
+            cv2.waitKey(1)
+
 except KeyboardInterrupt:
     pass
